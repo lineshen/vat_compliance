52bf64b8cc72b5d8dc65523b3f369f0264964ac8
lineshenm <lineshenm@gmail.com>
2018-03-06 23:54:00 +0200

    This is the first commit

---
+++ vat_compliance/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vat_compliance.MvcApplication" Language="C#" %>
---
+++ vat_compliance/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vat_compliance.Startup))]
+namespace vat_compliance
+{
+  public partial class Startup
+  {
+    public void Configuration(IAppBuilder app)
+    {
+      ConfigureAuth(app);
+    }
+  }
+}
